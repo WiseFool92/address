4175a142549d0f51ebaec5ee0e9207324d48a788
Nathan Watkins Hoagland <watkins92@gmail.com>
2020-04-07 08:21:38 -0700

    frontend ui interactivity w/js

--- js/scripts.js
+++ js/scripts.js
@@ -54,13 +54,50 @@ Contact.prototype.update = function() {
   return this.firstName + " " + this.lastName + " " + this.phoneNumber;
 }
 
-var addressBook = new Addressbook();
-var contact = new Contact("Ada", "Lovelace", "503-555-0100");
-var contact2 = new Contact("Grace", "Hopper", "503-555-0199");
-addressBook.addContact(contact);
-addressBook.addContact(contact2);
-
 // Frontend UI
+
+var addressBook = new Addressbook();
+
+function dispplayContactDetails(addressBookToDisplay) {
+  var contactsList = $("ul#contacts");
+  var htmlForContactInfo = "";
+  addressBookToDisplay.contacts.forEach(function(contact) {
+    htmlForContactInfo += "<li id=" + contact.id + ">" + contact.firstName + " " + contact.lastName + "</li>";
+  });
+  contactsList.html(htmlForContactInfo);
+}
+
+function showContact(contactId) {
+  var contact = addressBook.findContact(contactId);
+  $("#show-contact").show();
+  $(".first-name").html(contact.firstName);
+  $(".last-name").html(contact.lastName);
+  $(".phone-number").html(contact.phoneNumber);
+  var buttons = $("#buttons");
+  buttons.empty();
+  buttons.append("<button class='deleteButton' id=" + + contact.id + ">Delete</button>");
+}
+
+function attachContactListeners() {
+  $("ul#contacts").on("click", "li", function() {
+    showContact(this.id);
+  });
+  $("#buttons").on("click", ".deleteButton", function() {
+    addressBook.deleteContact(this.id);
+    $("#show-contact").hide();
+    dispplayContactDetails(addressBook);
+  });
+};
+
 $(document).ready(function() {
-  event.preventDefault();
+  attachContactListeners();
+  $("form#new-contact").submit(function(event) {
+      event.preventDefault();
+      var inputtedFirstName = $("input#new-first-name").val();
+      var inputtedLastName = $("input#new-last-name").val();
+      var inputtedPhoneNumber = $("input#new-phone-number").val();
+      var newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedPhoneNumber);
+      addressBook.addContact(newContact);
+      dispplayContactDetails(addressBook);
+    })
 })
